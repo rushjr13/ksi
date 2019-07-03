959e0ebbf755f553e3536fa070551d0a611fe8bd
Ruslan Samuel <ruslansamuel11@gmail.com>
2019-07-03 11:33:56 +0800

    mengubah template admin/topbar.php

--- application/views/template/admin/topbar.php
+++ application/views/template/admin/topbar.php
@@ -51,14 +51,15 @@
             <!-- Nav Item - Alerts -->
             <li class="nav-item dropdown no-arrow mx-1">
               <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <!-- Counter - Alerts -->
                 <?php
+                  $this->db->where('baca', 0);
                   $jlh_pemberitahuan=$this->db->count_all_results('pemberitahuan');
                 ?>
                 <?php if($jlh_pemberitahuan<1){ ?>
                   <i class="fas fa-bell fa-fw"></i>
                 <?php } else { ?>
                   <i class="fas fa-bell fa-fw"></i>
-                  <!-- Counter - Alerts -->
                   <span class="badge badge-danger badge-counter"><?=$jlh_pemberitahuan ?></span>
                 <?php } ?>
 
@@ -109,7 +110,7 @@
                         }
 
                         $tgl_pemberitahuan = $tgl.' '.$bulan.' '.$thn;
-                        $tgl_pemberitahuan2 = $tgl.' '.$bulan.' '.$thn.' - '.$jam.':'.$mnt.':'.$dtk.' '.$saat;
+                        $tgl_pemberitahuan2 = $tgl.' '.$bulan.' '.$thn.' - '.$jam.':'.$mnt.':'.$dtk;
                       ?>
                       <div class="small text-gray-500"><?=$tgl_pemberitahuan2 ?></div>
                       <?php if($berita['baca']==0){ ?>
@@ -124,61 +125,84 @@
               </div>
             </li>
 
-            <!-- Nav Item - Messages -->
-            <li class="nav-item dropdown no-arrow mx-1">
-              <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-envelope fa-fw"></i>
-                <!-- Counter - Messages -->
-                <span class="badge badge-danger badge-counter">7</span>
-              </a>
-              <!-- Dropdown - Messages -->
-              <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
-                <h6 class="dropdown-header">
-                  Message Center
-                </h6>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt="">
-                    <div class="status-indicator bg-success"></div>
-                  </div>
-                  <div class="font-weight-bold">
-                    <div class="text-truncate">Hi there! I am wondering if you can help me with a problem I've been having.</div>
-                    <div class="small text-gray-500">Emily Fowler · 58m</div>
-                  </div>
+            <?php if($pengguna_masuk['id_level']==1){ ?>
+              <!-- Nav Item - Messages -->
+              <li class="nav-item dropdown no-arrow mx-1">
+                <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <!-- Counter - Messages -->
+                  <?php
+                    $this->db->where('status_em', 0);
+                    $jlh_email=$this->db->count_all_results('email_masuk');
+                  ?>
+                  <?php if($jlh_email<1){ ?>
+                    <i class="fas fa-envelope fa-fw"></i>
+                  <?php } else { ?>
+                    <i class="fas fa-envelope fa-fw"></i>
+                    <span class="badge badge-danger badge-counter"><?=$jlh_email ?></span>
+                  <?php } ?>
                 </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/AU4VPcFN4LE/60x60" alt="">
-                    <div class="status-indicator"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">I have the photos that you ordered last month, how would you like them sent to you?</div>
-                    <div class="small text-gray-500">Jae Chun · 1d</div>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/CS2uCrpNzJY/60x60" alt="">
-                    <div class="status-indicator bg-warning"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">Last month's report looks great, I am very happy with the progress so far, keep up the good work!</div>
-                    <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                  </div>
-                </a>
-                <a class="dropdown-item d-flex align-items-center" href="#">
-                  <div class="dropdown-list-image mr-3">
-                    <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60" alt="">
-                    <div class="status-indicator bg-success"></div>
-                  </div>
-                  <div>
-                    <div class="text-truncate">Am I a good boy? The reason I ask is because someone told me that people say this to all dogs, even if they aren't good...</div>
-                    <div class="small text-gray-500">Chicken the Dog · 2w</div>
-                  </div>
-                </a>
-                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
-              </div>
-            </li>
+                <!-- Dropdown - Messages -->
+                <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
+                  <h6 class="dropdown-header">
+                    Email / Pesan Masuk
+                  </h6>
+                  <?php foreach ($email_masuk as $em): ?>
+                    <!-- Tanggal Indonesia Berdasarkan Fungsi time() - 1560990566 -->
+                    <?php 
+                      date_default_timezone_set('Asia/Makassar');
+                      $tgl = date('d', $em['tgl_em']);
+                      $bln = date('F', $em['tgl_em']);
+                      $thn = date('Y', $em['tgl_em']);
+                      $jam = date('H', $em['tgl_em']);
+                      $mnt = date('i', $em['tgl_em']);
+                      $dtk = date('s', $em['tgl_em']);
+                      $saat = date('a', $em['tgl_em']);
+
+                      if($bln=='January'){
+                          $bulan='Januari';
+                      } else if($bln=='February'){
+                          $bulan='Februari';
+                      } else if($bln=='March'){
+                          $bulan='Maret';
+                      } else if($bln=='April'){
+                          $bulan='April';
+                      } else if($bln=='May'){
+                          $bulan='Mei';
+                      } else if($bln=='June'){
+                          $bulan='Juni';
+                      } else if($bln=='July'){
+                          $bulan='Juli';
+                      } else if($bln=='August'){
+                          $bulan='Agustus';
+                      } else if($bln=='September'){
+                          $bulan='September';
+                      } else if($bln=='October'){
+                          $bulan='Oktober';
+                      } else if($bln=='November'){
+                          $bulan='November';
+                      } else if($bln=='December'){
+                          $bulan='Desember';
+                      }
+
+                      $tgl_em = $tgl.' '.$bulan.' '.$thn;
+                      $tgl_em2 = $tgl.' '.$bulan.' '.$thn.' - '.$jam.':'.$mnt.':'.$dtk;
+                    ?>
+                    <a class="dropdown-item d-flex align-items-center" href="<?=base_url('pesan/lihat/').$em['id_em'] ?>">
+                      <div class="font-weight-bold">
+                        <div class="small text-gray-500"><?=$em['nama_pengirim'] ?> (<?=$em['email_pengirim'] ?>)</div>
+                        <?php if($em['status_em']==0){ ?>
+                          <span class="font-weight-bold"><?=$em['isi_em'] ?></span>
+                        <?php }else{ ?>
+                          <span><?=$em['isi_em'] ?></span>
+                        <?php } ?>
+                        <div class="small text-gray-500"><?=$tgl_em2 ?></div>
+                      </div>
+                    </a>
+                  <?php endforeach ?>
+                  <a class="dropdown-item text-center small text-gray-500" href="<?=base_url('pesan') ?>">Lihat Semua Pesan</a>
+                </div>
+              </li>
+            <?php } ?>
 
             <div class="topbar-divider d-none d-sm-block"></div>
 
